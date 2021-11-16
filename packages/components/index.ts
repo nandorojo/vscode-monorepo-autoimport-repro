57b93cc7c5a70ddc21a5d97cf8f6a465d3482319
Fernando Rojo <frojo@sas.upenn.edu>
2021-11-16 13:55:46 -0500

    remove deps

--- packages/components/index.ts
+++ packages/components/index.ts
@@ -1,5 +1,3 @@
-export const testHere = 1;
-
 // 🚨 this doesn't autoimport from 'react'
 // even though 'react' is a dependency of 'apps/nextjs'
 // if you add "react: *" to "peerDependencies"/"devDependencies" of root package.json, it works
